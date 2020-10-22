a1b4c239916d9b928b26eccf2f8a6f47f07b4250
TechySharnav <sharnav123@gmail.com>
2020-10-22 11:36:39 +0530

    Completed the Project. Fully Working

--- App.js
+++ App.js
@@ -1,21 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, Image, TouchableOpacity, Stylesheet } from "react-native";
+import * as Permissions from "expo-permissions";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasCameraPermission: null,
+      isScanned: false,
+      ScannedData: "",
+      buttonState: "normal",
+    };
+  }
+
+  getCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    this.setState({
+      hasCameraPermission: status === "granted",
+    });
+  };
+
+  handleBarcodeScan = (res) => {
+    console.log("Scan in Progress...");
+    this.setState({
+      isScanned: false,
+      ScannedData: res.data,
+      buttonState: "normal",
+    });
+  };
+
+  render() {
+    var permissionGranted = this.state.hasCameraPermission;
+    var isScanned = this.state.isScanned;
+    var buttonState = this.state.buttonState;
+
+    if (buttonState === "clicked" && permissionGranted) {
+      return (
+        <View>
+          <Text style={{ marginTop: 50, textAlign: "center" }}>
+            SCAN IN PROGRESS...
+          </Text>
+          <BarCodeScanner
+            style={{
+              display: "flex",
+              alignSelf: "center",
+              justifyContent: "center",
+              width: "90%",
+              height: "90%",
+              marginTop: -50,
+            }}
+            onBarCodeRead={isScanned ? undefined : this.handleBarcodeScan}
+            onBarCodeScanned={isScanned ? undefined : this.handleBarcodeScan}
+          ></BarCodeScanner>
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <Text style={{ fontSize: 18, textAlign: "center", marginTop: 100 }}>
+            Scan a Barcode/QR Code
+          </Text>
+          <Image
+            source={require("./assets/barcodeScanner.png")}
+            style={{
+              alignSelf: "center",
+              width: "40%",
+              height: "40%",
+              resizeMode: "contain",
+              marginRight: 20,
+              marginTop: 30,
+            }}
+          />
+          <TouchableOpacity
+            onPress={() => {
+              this.getCameraPermission();
+              this.setState({ buttonState: "clicked" });
+            }}
+            style={{
+              backgroundColor: "#1e1e1e",
+              borderRadius: 6,
+              marginTop: 50,
+              width: "40%",
+              alignSelf: "center",
+            }}
+          >
+            <Text style={{ textAlign: "center", color: "white" }}>SCAN</Text>
+          </TouchableOpacity>
+          <Text style={{ textAlign: "center", marginTop: 70 }}>
+            {permissionGranted ? this.state.ScannedData : "No Data to show."}
+          </Text>
+        </View>
+      );
+    }
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
